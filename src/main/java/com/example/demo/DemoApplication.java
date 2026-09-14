4ccf3799f4d558130c8a9e8c138e9155473b40e9
Sudhanshu Pathak <sudhanshupathak.connect@gmail.com>
2026-09-14 20:29:36 +0530

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext contex = SpringApplication.run(DemoApplication.class, args);
+		PaymentGateway paymentGateway = contex.getBean(PaymentGateway.class);
+//		 paymentGateway.setType("RazorPay ");
+//		 paymentGateway.setRetryCount(5 );
 
 		System.out.println("Hello World!");
+		System.out.println(paymentGateway.getType());
+		System.out.println(paymentGateway.getRetryCount());
+		System.out.println(paymentGateway.isEnabled());
+		System.out.println(paymentGateway.getTimeout());
 	}
 
 }
